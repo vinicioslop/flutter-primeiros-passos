2d65fa0f6c35938508f6a1c45d21bafb7651c2ca
vinicioslop <vinicioslop7@hotmail.com>
2021-10-10 14:51:54 -0300

    Implementa listagem de contatos no lista_dio

--- lista_dio/lib/main.dart
+++ lista_dio/lib/main.dart
@@ -29,39 +29,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Contato> contatos = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    contatos.add(Contato(
+        nome: "João", telefone: "98798-7777", tipo: ContatoType.CELULAR));
+    contatos.add(Contato(
+        nome: "Lucas", telefone: "99879-7777", tipo: ContatoType.CELULAR));
+    contatos.add(Contato(
+        nome: "Maria", telefone: "99879-7777", tipo: ContatoType.FAVORITO));
+    contatos.add(
+        Contato(nome: "Thiago", telefone: "3379-9999", tipo: ContatoType.CASA));
+    contatos.add(Contato(
+        nome: "Mariana", telefone: "3379-9999", tipo: ContatoType.CASA));
+    contatos.add(Contato(
+        nome: "Beatriz", telefone: "98887-7777", tipo: ContatoType.FAVORITO));
+    contatos.add(Contato(
+        nome: "Liz", telefone: "3361-9999", tipo: ContatoType.TRABALHO));
+    contatos.add(Contato(
+        nome: "Matheus", telefone: "3344-9999", tipo: ContatoType.TRABALHO));
+
+    contatos.sort((a, b) => a.nome.compareTo(b.nome));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: ListView.separated(
+            itemBuilder: (context, index) {
+              var contato = contatos[index];
+              return ListTile(
+                leading: CircleAvatar(
+                  child: ContatoHelper.getIconByContatoType(contato.tipo),
+                  backgroundColor: Colors.blue[200],
+                ),
+                title: Text(contato.nome),
+                subtitle: Text(contato.telefone),
+                trailing: IconButton(
+                  icon: Icon(Icons.call),
+                  onPressed: () => {},
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => Divider(),
+            itemCount: contatos.length));
+  }
+}
+
+class Contato {
+  final String nome;
+  final String telefone;
+  final ContatoType tipo;
+
+  Contato({required this.nome, required this.telefone, required this.tipo});
+}
+
+enum ContatoType { CELULAR, TRABALHO, FAVORITO, CASA }
+
+class ContatoHelper {
+  static Icon getIconByContatoType(ContatoType tipo) {
+    switch (tipo) {
+      case ContatoType.CELULAR:
+        return Icon(Icons.phone_android, color: Colors.green[700]);
+      case ContatoType.TRABALHO:
+        return Icon(Icons.work, color: Colors.brown[600]);
+      case ContatoType.FAVORITO:
+        return Icon(Icons.star, color: Colors.yellow[600]);
+      case ContatoType.CASA:
+        return Icon(Icons.home, color: Colors.purple[600]);
+    }
   }
 }
